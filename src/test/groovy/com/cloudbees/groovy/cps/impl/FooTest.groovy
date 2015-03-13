dfdba8bb70e26fdff4fd5e425519b1d751f08c8d
tfennelly <tom.fennelly@gmail.com>
2015-03-13 11:47:59 +0000

    Added caller check

--- src/test/groovy/com/cloudbees/groovy/cps/impl/FooTest.groovy
+++ src/test/groovy/com/cloudbees/groovy/cps/impl/FooTest.groovy
@@ -1,13 +1,16 @@
 package com.cloudbees.groovy.cps.impl
 
+import org.junit.Test
+
 public class FooTest {
 
+    @Test
     public void test1() {
         assert 4==add(1,3);
     }
 
     public int add(int a, int b) { // CPS transformed version
-        if (???) {
+        if (Caller.isAsynchronous(this, "add", [a,b])) {
             throw new CpsCallableInvocation(null /*dummy*/ ,this, [a,b]);
         } else {
             return a+b; //
